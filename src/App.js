bfc76a7da3ae5f4d7f3739a7c10a6dc8de9df9bc
ambhar <amol.freelancing@gmail.com>
2022-06-08 14:35:26 +0530

    adding more section

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Search from "./components/Search/Search";
 import Card from "./components/Card/Card";
 import Pagination from "./components/Pagination/Pagination";
 import Filter from "./components/Filter/Filter";
 import Navbar from "./components/Navbar/Navbar";
+import Episodes from "./Pages/Episodes";
+import Location from "./Pages/Location";
 
 
 
-function App() {
-
+const Home = () => {
   let [fetchedData, updateFetchedData] = useState([]);
+  let [pageNumber, updatePageNumber] = useState(1);
+  let [search, setSearch] = useState("");
+  let [status, updateStatus] = useState("");
+  let [gender, updateGender] = useState("");
+  let [species, updateSpecies] = useState("");
   let { info, results } = fetchedData;
 
-  let api = `https://rickandmortyapi.com/api/character/?page=1`
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
 
   useEffect(() => {
     (async function () {
@@ -27,11 +34,18 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="text-center mb-3">Characters</h1>
+      <h1 className="text-center mb-3">Rick Morty <span className="text-danger">Characters</span></h1>
+      <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
       <div className="container">
         <div className="row">
-          {/* <Filter /> */}
-          Filter
+          <Filter
+            pageNumber={pageNumber}
+            status={status}
+            updateStatus={updateStatus}
+            updateGender={updateGender}
+            updateSpecies={updateSpecies}
+            updatePageNumber={updatePageNumber}
+          />
           <div className="col-lg-8 col-12">
             <div className="row">
               <Card results={results} />
@@ -39,7 +53,31 @@ function App() {
           </div>
         </div>
       </div>
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        updatePageNumber={updatePageNumber}
+      />
     </div>
+  );
+}
+
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/episodes" element={<Episodes />} />
+
+        <Route path="/location" element={<Location />} />
+      </Routes>
+    </Router>
   );
 }
 
